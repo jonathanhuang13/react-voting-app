24262983cfaba5f0111c829930a3e6421302d7bc
Jonathan Huang <jonathanhuang@secondspectrum.com>
2016-06-07 09:33:23 -0700

    minor bug fix

--- src/reducer.js
+++ src/reducer.js
@@ -3,7 +3,7 @@ import { setEntries, next, vote, INITIAL_STATE } from './core';
 export default function reducer(state = INITIAL_STATE, action) {
   switch (action.type) {
     case 'SET_ENTRIES':
-      return setEntries(state, aciton.entries);
+      return setEntries(state, action.entries);
     case 'NEXT':
       return next(state);
     case 'VOTE':
